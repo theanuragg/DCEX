cbc7106348708cff623040df181648d7b1b6c331
Anurag Singh <anurags9693@gmail.com>
2025-03-31 13:25:52 +0500

    Add custom 404 page and SVG asset: create a user-friendly error page with a link to the homepage and include a corresponding SVG graphic for visual appeal.

---
+++ app/not-found.tsx
@@ -0,0 +1,21 @@
+'use client'
+// pages/404.js
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Custom404() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-dark2">
+      <Image src='./404.svg'  width={300} height={100} alt=""></Image>
+      <p className="mt-4 px-4 text-2xl text-gray-600">
+        Oops! The page you're looking for doesn't exist.
+      </p>
+      <Link 
+        href="/" 
+        className="mt-6 px-4 py-2 bg-primary text-dark3 rounded hover:bg-secondary  transition-colors"
+      >
+       Go Home 
+      </Link>
+    </div>
+  );
+}

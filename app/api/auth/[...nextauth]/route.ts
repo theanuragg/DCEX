1e51ebf270619fae7bd9246b3260efca51b7d6a4
Anurag Singh <anurags9693@gmail.com>
2025-04-05 02:08:04 +0500

    Refactor authentication setup: uncomment NextAuth configuration and export statements, and update keypair generation to use @solana/web3.js for improved key management.

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,10 @@
-// import NextAuth from "next-auth";
-// import { Authconfig } from "@/app/lib/auth";
+import NextAuth from "next-auth";
+import { Authconfig } from "@/app/lib/auth";
 
-// const handler = NextAuth(Authconfig)
+const handler = NextAuth(Authconfig)
 
 
-// export { handler as GET, handler as POST };
+export { handler as GET, handler as POST };
 
-console.log(process.env.GOOGLE_CLIENT_ID )
-console.log(process.env.GOOGLE_CLIENT_SECRET)
+// console.log(process.env.GOOGLE_CLIENT_ID )
+// console.log(process.env.GOOGLE_CLIENT_SECRET)

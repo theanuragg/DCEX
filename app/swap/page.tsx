a7fa72d387238e94d5bed029c9d50de4d3d651cc
Anurag Singh <anurags9693@gmail.com>
2025-05-15 23:59:54 +0500

    Implement code changes to enhance functionality and improve performance

---
+++ app/swap/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+// import JupiterSwapForm from "../Components/jupiter/swap";
+
+
+export default function Swap() {
+    return (
+        <div className="items-center justify-center font-normal text-2xl text-white">
+            Swap Component
+            {/* <JupiterSwapForm/> */}
+        </div>
+    );
+}

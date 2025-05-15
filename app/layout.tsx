d2eea516633ce1637ad2c952c78fe15cdf72abd6
Anurag Singh <anurags9693@gmail.com>
2025-05-15 23:58:28 +0500

    Refactor component imports to use Landing directory for consistency

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import  Appbar  from "./Components/Appbar";
+import  Appbar  from "./Components/Landing/Appbar";
 import { Providers } from "./provider";
 import { ThemeProvider } from "./Components/themeProvider";
 
@@ -18,6 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+  
     <html lang="en">
       <body className={inter.className} >
         <ThemeProvider attribute="class" defaultTheme="dark">
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,17 @@
 'use client'
 
-import BenefitComp from "./Components/BenefitComp";
-import Index from "./Components/FeatureCard";
-import Hero from "./Components/Hero";
-import Marquee from "./Components/marquee";
-import ServiceComp from "./Components/Service";
-import FaqAccordion from "./Components/FaqComp";
-import UpgradeComp from "./Components/Upgarde";
-import Footer from "./Components/Footer";
+import BenefitComp from "./Components/Landing/BenefitComp";
+import Index from "./Components/Landing/FeatureCard";
+import Hero from "./Components/Landing/Hero";
+import ServiceComp from "./Components/Landing/Service";
+import FaqAccordion from "./Components/Landing/FaqComp";
+import UpgradeComp from "./Components/Landing/Upgarde";
+import Footer from "./Components/Landing/Footer";
 
 export default function Home() {
   return (
    <main>
     <Hero/>
-    <Marquee/>
     <Index/>
     <BenefitComp/>
     <ServiceComp/>
